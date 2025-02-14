ae1abdf63322e2c7b696026ae5da53a8dccd03a3
David Carr <dave@dcblog.dev>
2025-02-14 21:18:17 +0000

    added scripts

--- config/sentemails.php
+++ config/sentemails.php
@@ -4,7 +4,7 @@
  * You can place your custom package configuration in here.
  */
 return [
-    //set the route path to load the sent emails ui defaults to /sentemails
+    // set the route path to load the sent emails ui defaults to /sentemails
     'routepath' => env('SENT_EMAILS_ROUTE_PATH', 'sentemails'),
 
     // set the route middlewares to apply on the sent emails ui
